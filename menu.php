a1160fd35d58f65b7368a33e9a282182eafa297d
gujinyu <929315680@qq.com>
2016-10-20 15:51:53 +0800

    1.0

---
+++ menu.php
@@ -0,0 +1,15 @@
+<?php
+	$conn=new MySqli("localhost","root","root","gjy");
+	$sql="select * from takeout_food";
+	$result=mysqli_query($conn, $sql);
+	$arr=array();
+	$index=0;
+	if($result->num_rows>0){
+		while($row = $result->fetch_assoc()){
+			$arr[$index]=$row;
+			$index++;
+		}
+	};
+	//echo var_dump($arr);
+	echo json_encode($arr,JSON_UNESCAPED_UNICODE);
+?>
